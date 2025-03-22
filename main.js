21a5e44dcc2f15f46fba6d3db8991287f997c7dd
Yaseen Al <yhalsalamy@gmail.com>
2025-03-21 21:19:21 -0400

    guesses show up in boxes

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
-let goBtn = document.getElementById("go-button")
-let inputGuessEle = document.getElementById("input-guess")
+const goBtn = document.getElementById("go-button")
+const inputGuessEle = document.getElementById("input-guess")
+const row1Ele = document.getElementById("row-1")
+const row2Ele = document.getElementById("row-2")
+const row3Ele = document.getElementById("row-3")
+const row4Ele = document.getElementById("row-4")
+const row5Ele = document.getElementById("row-5")
+const row6Ele = document.getElementById("row-6")
+let previousVal = ""
+let guessTurn = 0
+
+inputGuessEle.addEventListener("input", handleInputChange)
+goBtn.addEventListener("click", handleInputGuess)
 
 
-
-function handleInputGuess(){
-    // console.log(inputGuessEle)
-    console.log(inputGuessEle.value)
+function submitGuess(rowElement){
+    for(let i = 0; i < rowElement.children.length; i++){
+        rowElement.children[i].innerText = inputGuessEle.value.charAt(i)
+    }
+    guessTurn++
 }
 
 function isValidInput(str){
     // A valid input is all uppercased letters only 
-    if (str === ""){
-        return false
-    }
     for (let i = 0; i < str.length; i++){
         let currentChar = str.charAt(i)
         let asciiCode = currentChar.charCodeAt(0)
@@ -24,16 +33,34 @@ function isValidInput(str){
     
 }
 
-function handleInputChange(event){
-    if (!isValidInput(event.target.value)) {
-        
+function handleInputGuess(){
+    if (inputGuessEle.value.length !== 5){
+        return
     }
-    
-    console.log(event)
-    event.target.value = event.target.value.toUpperCase()
+    if (guessTurn === 0){
+        submitGuess(row1Ele)
+    } else if ( guessTurn === 1) {
+        submitGuess(row2Ele)
+    }else if ( guessTurn === 2) {
+        submitGuess(row3Ele)
+    }else if ( guessTurn === 3) {
+        submitGuess(row4Ele)
+    }else if ( guessTurn === 4) {
+        submitGuess(row5Ele)
+    }else if ( guessTurn === 5) {
+        submitGuess(row6Ele)
+    }
+    // console.log(inputGuessEle.value)
 }
 
-inputGuessEle.addEventListener("input", handleInputChange)
+function handleInputChange(event){
+    event.target.value = event.target.value.toUpperCase()
+    if (!isValidInput(event.target.value)) {
+        event.target.value = previousVal
+        return
+    }
+    previousVal = event.target.value
+}
 
 
-goBtn.addEventListener("click", handleInputGuess)
+// console.log(row1Ele.children)
